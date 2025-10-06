69d66d68106a4e18c2b712684147ca82087cba87
Becca Berger <berger.becca@gmail.com>
2025-10-06 23:47:56 +0100

    Initial Commit

---
+++ Lab2_MemoryGame_BeccaBerger_PatriciaOrtiz/Lab2_MemoryGame_BeccaBerger_PatriciaOrtizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2_MemoryGame_BeccaBerger_PatriciaOrtizApp.swift
+//  Lab2_MemoryGame_BeccaBerger_PatriciaOrtiz
+//
+//  Created by Zachary Berger on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Lab2_MemoryGame_BeccaBerger_PatriciaOrtizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
